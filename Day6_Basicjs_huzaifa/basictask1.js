a00f01456e55bdf5a34f67efaf79777cc11530b8
huzaifashaikh-56 <hs7030342@gmail.com>
2025-06-09 12:14:29 +0530

    Update basictask1.js

--- Day6_Basicjs_huzaifa/basictask1.js
+++ Day6_Basicjs_huzaifa/basictask1.js
@@ -1,1 +1,17 @@
+let myName = "Huzaifa";          
+const myAge = 21;            
+var isStudent = true;       
+let subjects = ["Math", "CS", "Physics"];  
+const student = {                
+  name: myName,
+  age: myAge,
+  isStudent: isStudent,
+  subjects: subjects
+};
 
+console.log("Task 1: Variables and Data Types");
+console.log("Name:", myName);
+console.log("Age:", myAge);
+console.log("Is Student:", isStudent);
+console.log("Subjects:", subjects);
+console.log("Student Object:", student);
