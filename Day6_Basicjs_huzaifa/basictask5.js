b96dc41654fba2f351990a7135f214c1da9fe741
huzaifashaikh-56 <hs7030342@gmail.com>
2025-06-09 16:52:41 +0530

    Create basictask5.js

---
+++ Day6_Basicjs_huzaifa/basictask5.js
@@ -0,0 +1,15 @@
+
+
+console.log("\nTask 5: Functions");
+
+function greetUser(name) {
+  console.log("Welcome, " + name + "!");
+}
+
+function square(num) {
+  return num * num;
+}
+
+// Example usage
+greetUser("Huzaifa");
+console.log("Square of 6 is:", square(6));
