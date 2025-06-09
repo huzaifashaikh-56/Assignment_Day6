2fc7d37f7105f2c02a9b5820558164be6f8ab71e
huzaifashaikh-56 <hs7030342@gmail.com>
2025-06-09 16:33:28 +0530

    Create basictask2.js

---
+++ Day6_Basicjs_huzaifa/basictask2.js
@@ -0,0 +1,22 @@
+
+
+let num1 = parseFloat(prompt("Enter first number:"));
+let num2 = parseFloat(prompt("Enter second number:"));
+
+let sum = num1 + num2;
+let diff = num1 - num2;
+let prod = num1 * num2;
+let div = num1 / num2;
+
+console.log("\nTask 2: Simple Arithmetic Calculator");
+console.log("Sum:", sum);
+console.log("Difference:", diff);
+console.log("Product:", prod);
+console.log("Division:", div);
+
+alert(
+  "Sum: " + sum +
+  "\nDifference: " + diff +
+  "\nProduct: " + prod +
+  "\nDivision: " + div
+);
