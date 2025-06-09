190e9e703b7c6ab0b447bdd541c53156e9bf37c9
huzaifashaikh-56 <hs7030342@gmail.com>
2025-06-09 16:48:06 +0530

    Create basictask4.js

---
+++ Day6_Basicjs_huzaifa/basictask4.js
@@ -0,0 +1,13 @@
+
+
+console.log("\nTask 4: For Loop (1 to 20)");
+for (let i = 1; i <= 20; i++) {
+  console.log(i);
+}
+
+console.log("\nTask 4: While Loop (Even numbers 2 to 10)");
+let even = 2;
+while (even <= 10) {
+  console.log(even);
+  even += 2;
+}
